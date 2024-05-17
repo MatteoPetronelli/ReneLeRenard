390d155f8933bfdbacda5659445fe6f0dada148a
MatteoPetronelli <matteo.petronelli5@gmail.com>
2024-05-17 19:33:02 +0200

    commit

--- Assets/scripts/platforms/MovingPlatform.cs
+++ Assets/scripts/platforms/MovingPlatform.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    [SerializeField] private Transform pos1Inspect;
+    [SerializeField] private Transform pos2Inspect;
     public Vector2 pos1;
     public Vector2 pos2;
     public float speed = 1f;
+
+    private void Start()
+    {
+        pos1 = pos1Inspect.position;
+        pos2 = pos2Inspect.position;
+    }
 
     private void Update()
     {
